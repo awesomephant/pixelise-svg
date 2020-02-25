74f0fb525bf3998f9aa56815cc3632f3aff76e33
Max Kohler <max@koehler-kn.de>
2020-02-25 11:20:46 +0000

    Updates

---
+++ precache-manifest.524d50ec20556cca26b75f5eb304d3e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a95cecd07e4cc9b3eab5cb6553e3ec",
+    "url": "/pixelise-svg/index.html"
+  },
+  {
+    "revision": "644dc8aa31874eaae071",
+    "url": "/pixelise-svg/static/css/main.04ef7a9d.chunk.css"
+  },
+  {
+    "revision": "b18f89d5e9542d8b374c",
+    "url": "/pixelise-svg/static/js/2.24f33357.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pixelise-svg/static/js/2.24f33357.chunk.js.LICENSE"
+  },
+  {
+    "revision": "644dc8aa31874eaae071",
+    "url": "/pixelise-svg/static/js/main.08f95ff5.chunk.js"
+  },
+  {
+    "revision": "8294f226a651a72e146f",
+    "url": "/pixelise-svg/static/js/runtime-main.30637720.js"
+  }
+]);
